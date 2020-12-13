11c43c101464806c84a40f01679da75d690fc238
MaQueen <shusineko@icloud.com>
2020-12-13 16:35:15 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <section className="container">
         <h3>{people.length} birtjday today</h3>
         <List people={people} />
-        <button onClick={() => console.log("you cliked me")}>Clear All</button>
+        <button onClick={() => setPeople([])}>Clear All</button>
       </section>
     </main>
   );
